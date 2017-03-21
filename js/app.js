eebfd6bf3df58234aa2a609e9a3cb20aa5c4f7e5
Ivan Wu <flash662@gmail.com>
2017-03-21 13:35:45 +0800

    put js to folder

---
+++ js/app.js
@@ -0,0 +1,52 @@
+Vue.component('tissue', {
+    template: '#template-tissue',
+    props: ['data', 'index', 'cheapest'],
+    computed: {
+        subtotal: function() {
+            var total = parseFloat(this.data.price) / (parseInt(this.data.sheet) * parseInt(this.data.package));
+            return isNaN(total) ? '' : total.toFixed(5);
+        }
+    },
+    watch: {
+        subtotal: function () {
+            this.data.subtotal = this.subtotal;
+        }
+    }
+});
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        count: 5,
+        mockData: {
+            name: '',
+            sheet: '',
+            package: '',
+            price: '',
+            subtotal: ''
+        },
+        data: []
+    },
+    methods: {
+        addColumn: function() {
+            for(i=0; i<this.count; i++) {
+                this.data.push(Vue.util.extend({}, this.mockData));
+            }
+        },
+        isCheapest: function (data) {
+            return data.subtotal == this.cheapest;
+        }
+    },
+    computed: {
+        cheapest: function () {
+            var arr = $.map(this.data, function(data) {
+                return data.subtotal;
+            }).filter(Number);
+
+            return Math.min.apply(null, arr);
+        }
+    },
+    mounted: function () {
+        this.addColumn();
+    }
+});
